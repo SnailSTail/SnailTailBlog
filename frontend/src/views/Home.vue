483e25934ef99514f0be5f32e8900223c9db795b
qjl1201 <qjl_1201@163.comm>
2021-01-17 14:10:30 +0800

    project initialize

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,16 @@
+﻿<template>
+  <div className="container">
+    <div class="row justify-content-center py-5">
+      <h3>Home</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+};
+</script>
+
+<style scoped>
+</style>
